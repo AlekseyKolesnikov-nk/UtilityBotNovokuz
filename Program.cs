c22192f49b3c5a2350878da5766e9f64557b47d3
AlekseyKolesnikov-nk <Aleksey.Kolesnikov@evraz.com>
2024-10-27 22:02:00 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using UtilityBotNovokuz;
+
+namespace UtilityBotNovokuz
+{
+    static class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services))
+                .UseConsoleLifetime()
+                .Build();
+
+            Console.WriteLine("Starting Service");
+            await host.RunAsync();
+            Console.WriteLine("Service stopped");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("7615351480:AAG916o5vCqGy-2bNuMjRUel6JAdrDzgkAY"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
+        }
+    }
+}
+
+
+
+
